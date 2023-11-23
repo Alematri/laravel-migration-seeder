d384ac88303a8096519f4edf8f254440b9aae37e
Alematri <alessandromatricardi@gmail.com>
2023-11-23 10:59:31 +0100

    [fix]

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -19,7 +20,7 @@ class TrainsTableSeeder extends Seeder
         for($i = 0; $i < 10; $i++){
             $train = new Train();
             $train->reference = $faker->words(3, true);
-            $train->slug = $this->generateSlug($train->reference);
+            //$train->slug = $this->generateSlug($train->reference);
             $train->azienda = $faker->randomElement(["Trenitalia", "Italo", "Trenino Ciuff Ciuff", "Freccia Bianca", "Freccia Argento", "Freccia Rossa"]);
             $train->stazione_di_partenza = $faker->city;
             $train->stazione_di_arrivo = $faker->city;
@@ -27,8 +28,8 @@ class TrainsTableSeeder extends Seeder
             $train->orario_di_arrivo = $faker->time;
             $train->codice_treno = $faker->bothify("??###");
             $train->numero_carrozza = $faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-            $train->in_orario = $faker->randomElement(["Y", "N"]);
-            $train->cancellato = $faker->randomElement(["Y", "N"]);
+            $train->in_orario = $faker->randomElement([0, 1]);
+            $train->cancellato = $faker->randomElement([0, 1]);
 
             $train->save();
         }
