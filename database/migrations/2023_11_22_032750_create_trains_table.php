ba4c62075e459b2b8c9410ee85027d76a6e692c5
Alematri <alessandromatricardi@gmail.com>
2023-11-23 03:58:11 +0100

    [Fake train Db]

--- database/migrations/2023_11_22_032750_create_trains_table.php
+++ database/migrations/2023_11_22_032750_create_trains_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string("reference", 100)->nullable();
+            $table->string("slug", 100)->nullable();
             $table->string('azienda',50);
             $table->string('stazione_di_partenza',50);
             $table->string('stazione_di_arrivo',50);
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 10; $i++){
+            $train = new Train();
+            $train->reference = $faker->words(3, true);
+            $train->slug = $this->generateSlug($train->reference);
+            $train->azienda = $faker->randomElement(["Trenitalia", "Italo", "Trenino Ciuff Ciuff", "Freccia Bianca", "Freccia Argento", "Freccia Rossa"]);
+            $train->stazione_di_partenza = $faker->city;
+            $train->stazione_di_arrivo = $faker->city;
+            $train->orario_di_partenza = $faker->time;
+            $train->orario_di_arrivo = $faker->time;
+            $train->codice_treno = $faker->bothify("??###");
+            $train->numero_carrozza = $faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            $train->in_orario = $faker->randomElement(["Y", "N"]);
+            $train->cancellato = $faker->randomElement(["Y", "N"]);
+
+            $train->save();
+        }
+    }
+
+    private function generateSlug($reference){
+		$slug = Str::slug($reference, "-");
+		$original_slug = $slug;
+		$exists = Train::where("slug", $slug)->first();
+		$c = 1;
+		while($exists){
+			$slug = $original_slug . "-" . $c;
+			$exists = Train::where("slug", $slug)->first();
+
+			$c++;
+		}
+		return $slug;
+	}
+}
